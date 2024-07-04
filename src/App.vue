394ea3f9d64f8aabfb40f9b520db363de840fca5
Emerson Batista <emerson.dev21@gmail.com>
2024-07-03 23:09:26 -0300

    upload Social-Links

---
+++ src/App.vue
@@ -0,0 +1,131 @@
+<script setup>
+
+import Item from "./components/item.vue"
+</script>
+
+<script>
+
+import Dados from "./data/data.json"
+export default {
+  data() {
+    return {
+      datas: null
+    }
+  },
+  mounted() {
+    this.datas = Dados
+    // console.log(Dados)
+  }
+}
+
+</script>
+
+<template>
+  <div>
+    <header>
+
+      <div class="profile">
+        <img src="./assets/profile.png" />
+      </div>
+
+      <h1>Emerson Batista</h1>
+
+    </header>
+    <hr />
+    <hr />
+    <div class="card">
+      <Item v-for="references, index in datas" :key="index" :item="references" />
+    </div>
+
+    <footer>
+      <h3>Emerson Batista - 2024</h3>
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+header {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 50px 0 20px 0;
+  color: #221E40;
+}
+
+.profile {
+  display: flex;
+  width: 170px;
+  border-radius: 100%;
+}
+
+.profile:hover {
+  border: solid 6px;
+}
+
+.profile img {
+  border-radius: 100%;
+  width: 100%;
+}
+
+h1 {
+  color: #221E40;
+  line-height: 1.5;
+  position: relative;
+}
+
+h1::before {
+  content: '';
+  width: 0;
+  height: 5px;
+  border-radius: 2px;
+  background-color: #221E40;
+  position: absolute;
+  bottom: -.25rem;
+  left: 50%;
+  transition: width .4s, left .4s;
+}
+
+h1:hover::before {
+  width: 100%;
+  left: 0;
+}
+
+.card {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: 50px 0;
+}
+
+footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  background: rgba(255, 255, 255, 0.22);
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(5.8px);
+  -webkit-backdrop-filter: blur(5.8px);
+  border: 1px solid rgba(255, 255, 255, 0.57);
+}
+
+@media (width <= 800px) {
+
+  h1 {
+    color: #E0E3F0;
+  }
+
+  .card {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  footer {
+    background-color: #E0E3F0;
+  }
+}
+
+</style>
---
+++ src/components/item.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <a :href="item.href">
+      <img :src="item.img" class="normal-Icon" alt="Icon">
+      <img :src="item.imgMobile" class="mobile-Icon" alt="Icon">
+      <span>{{ item.title }}</span>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['item']
+}
+</script>
+
+<!-- "scoped" faz com que esse "style funcione exclusivamente para esse componente" -->
+<style scoped>
+div {
+  border: solid 1px red;
+  width: 120px;
+  height: 150px;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  margin: 30px 20px;
+  border-radius: 16px;
+  cursor: pointer;
+  background: rgba(255, 255, 255, 0.438);
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(5.8px);
+  -webkit-backdrop-filter: blur(5.8px);
+  border: 1px solid rgba(255, 255, 255, 0.57);
+}
+
+div:hover {
+  transform: translate(0px, -16px)
+}
+
+img {
+  width: 80px;
+  align-self: center;
+}
+
+.mobile-Icon {
+  display: none;
+}
+
+span {
+  font-weight: 900;
+}
+
+@media (width <=800px) {
+
+  div {
+    width: 80%;
+    height: 70px;
+    margin: 10px 20px;
+    border-radius: 50px;
+    background-color: #E0E3F0;
+  }
+
+  a {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    text-align: center;
+  }
+
+  img {
+    width: 50px;
+    margin: 0 20px;
+  }
+
+  .normal-Icon {
+    display: none;
+  }
+
+  .mobile-Icon {
+    display: block;
+  }
+
+  span {
+    text-align: start;
+    font-size: 20px;
+    width: 70%;
+  }
+}
+</style>
